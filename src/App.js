3382071089537d4302389bba1675efb4d056f560
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-06-04 17:26:13 +0530

    AddMovie Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import AddMovie from "./component/AddMovie";
 import Card from "./component/Card";
 import Header from "./component/Header";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-     <Header/>
-     <Card/>
+      <Header/>
+      <Routes>
+         <Route path="/" element={<Card/>}></Route>
+         <Route path="/addmovie" element={<AddMovie/>}></Route>
+      </Routes>
+
     </div>
   );
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,17 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import ReactStars from 'react-stars'
 const Card = () => {
-  return (
-    <div className='flex flex-wrap justify-between p-3 mt-2'>
-    <div className='card font-medium shadow-lg p-1 hover:-translate-y-1 cursor-pointer'>
-        <img className='h-72' src='https://marketplace.canva.com/EAFMqwkPfOY/2/0/1131w/canva-black-minimalist-horror-movie-poster-3bttgZhMDnA.jpg'></img>
-        <h1><span className='text-gray-500'>Name: </span> The Dark</h1>
-        <h1> <span className='text-gray-500'>Rating: </span>5</h1>
-        <h1><span className='text-gray-500'>Year: </span>2009</h1>
-    </div>
-
-    </div>
-  )
+    const [data, setData] = useState([
+        {
+            name: "The Dark",
+            Year: "2008",
+            Rating: "5",
+            img: "https://marketplace.canva.com/EAFMqwkPfOY/2/0/1131w/canva-black-minimalist-horror-movie-poster-3bttgZhMDnA.jpg"
+        }
+    ])
+    return (
+        <div className='flex flex-wrap justify-between p-3 mt-2'>{
+            data.map((element, index) => {
+                return (
+                    <div key={index} className='card font-medium shadow-lg p-1 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500'>
+                        <img className='h-72' src={element.img}></img>
+                        <h1><span className='text-gray-500'>Name: </span> {element.name}</h1>
+                        <h1 className='flex items-center'> <span className='text-gray-500 mr-1' >Rating: </span>
+                        <ReactStars
+                            size={20}
+                            half={true}
+                            edit={false}
+                            value={element.Rating}
+                        
+                
+                        />
+                        </h1>
+                        <h1><span className='text-gray-500'>Year: </span>{element.Year}</h1>
+                    </div>
+                )
+            })
+        }
+        </div>
+    )
 }
 
 export default Card
